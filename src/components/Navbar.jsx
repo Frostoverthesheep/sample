0cc8f1a9faf9f646af33f9b998126bb05975f121
Frostoverthesheep <atanaciobasa9@gmail.com>
2023-10-01 19:27:25 +0800

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom"
+
+const Navbar = () => {
+  return (
+    <div className='bg-red-500 text-white flex justify-between p-5'>
+        <h1>POGI AKO</h1>
+        <div >
+            <ul className='font-bold flex space-x-5'>
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+                <Link to="">Portfolio</Link>
+                <Link to="">Contact Us</Link>
+            </ul>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/router/Route.js
@@ -0,0 +1,19 @@
+import {
+  createBrowserRouter,
+  Link,
+} from "react-router-dom";
+import Home from "../pages/Home";
+import About from "../pages/About";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "about",
+    element: <About />
+  },
+]);
+
+export default router;
